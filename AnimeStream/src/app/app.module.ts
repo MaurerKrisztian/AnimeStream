9112d3bfb01247fd33fc79ae5355342700dffddd
Maurer Krisztian <maurerkrisztian@gmail.com>
2020-09-05 19:39:18 +0200

    nebular theme

--- AnimeStream/src/app/app.module.ts
+++ AnimeStream/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { NbThemeModule, NbLayoutModule, NbCardModule, NbInputModule } from '@nebular/theme';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,13 @@ import { HomePageComponent } from './home-page/home-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'dark' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbCardModule,
+    NbInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
