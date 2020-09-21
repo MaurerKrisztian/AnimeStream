406d78141342e62a7f1ae4f5b850a6203055c3de
gerencserjani <webgjohn@gmail.com>
2020-09-21 19:04:19 +0200

    Main Menu card Design

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,34 +5,29 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css', './home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
   animes;
-  constructor(private api: ApiService, private route: ActivatedRoute) { }
+  constructor(private api: ApiService, private route: ActivatedRoute) {}
 
-  term = "";
+  term = '';
   ngOnInit(): void {
-
-    this.route.queryParams.subscribe(params => {
-      if(params['term'] != undefined){
+    this.route.queryParams.subscribe((params) => {
+      if (params['term'] != undefined) {
         this.term = params['term'];
-        console.log(this.term)
+        console.log(this.term);
       }
-      
+
       this.getAnimes(this.term);
-      console.log(this.term)
-  });
-
-
+      console.log(this.term);
+    });
   }
 
-  getAnimes(term){
-    this.api.get('/api/anime?term=' + term).subscribe((data)=>{
+  getAnimes(term) {
+    this.api.get('/api/anime?term=' + term).subscribe((data) => {
       this.animes = data;
-      console.log(data)
-  });
+      console.log(data);
+    });
   }
-
 }
