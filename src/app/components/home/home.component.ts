993c05a92c45cd855215f729ba896026860920f5
Maurer Krisztian <maurerkrisztian@gmail.com>
2020-09-21 22:11:00 +0200

    pagination

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { ActivatedRoute } from '@angular/router';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,25 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
   animes;
+
+  length = 100;
+  pageSize = 6;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
+
   constructor(private api: ApiService, private route: ActivatedRoute) {}
+
+  pageEvent: PageEvent;
+
+  pagination($event){
+    console.log("test", $event)
+
+    this.getAnimes(this.term, $event.pageIndex, $event.pageSize)
+  }
+  setPageSizeOptions(setPageSizeOptionsInput: string) {
+    if (setPageSizeOptionsInput) {
+      this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+    }
+  }
 
   term = '';
   ngOnInit(): void {
@@ -19,14 +38,15 @@ export class HomeComponent implements OnInit {
         console.log(this.term);
       }
 
-      this.getAnimes(this.term);
+      this.getAnimes(this.term, 1, this.pageSize);
       console.log(this.term);
     });
   }
 
-  getAnimes(term) {
-    this.api.get('/api/anime?term=' + term).subscribe((data) => {
-      this.animes = data;
+  getAnimes(term, pageIndex, pageSize) {
+    this.api.get('/api/anime?term=' + term + '&page='+pageIndex+'&limit='+ pageSize).subscribe((data: any) => {
+      this.animes = data.data;
+      this.length = data.meta.collectionSize
       console.log(data);
     });
   }
